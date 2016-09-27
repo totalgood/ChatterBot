4b0d6daeddb5092071760aeff441e2e9e0a10d0f
Hobson Lane <github@totalgood.com>
2016-09-26 22:25:28 -0700

    better admin

--- chatterbot/ext/django_chatterbot/admin.py
+++ chatterbot/ext/django_chatterbot/admin.py
@@ -4,14 +4,16 @@ from chatterbot.ext.django_chatterbot.models import Statement, Response, ChatUse
 
 @admin.register(Statement)
 class StatementAdmin(admin.ModelAdmin):
-    list_display = ('text', 'prompts', 'chat_user', 'created', 'modified', )
-    list_filter = ('text', 'prompts', 'chat_user', 'created', 'modified', )
-    search_fields = ('text', 'prompts', 'chat_user', 'created', 'modified', )
+    list_display = ('text', 'score', 'chat_user', 'created', 'modified', )
+    list_filter = ('text', 'score', 'chat_user', 'created', 'modified', )
+    search_fields = ('text', 'score', 'chat_user', 'created', 'modified', )
 
 
 @admin.register(Response)
 class ResponseAdmin(admin.ModelAdmin):
     list_display = ('response', 'prompt', 'occurrence', 'created', 'modified', )
+    list_filter = ('response', 'prompt', 'occurrence', 'created', 'modified', )
+    search_fields = ('response', 'prompt', 'occurrence', 'created', 'modified', )
 
 
 @admin.register(ChatUser)
