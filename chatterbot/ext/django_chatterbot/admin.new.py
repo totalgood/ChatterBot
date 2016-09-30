f037c95ab0ea8932f3f9b9aeef632f6c5fdc3dcb
Hobson Lane <github@totalgood.com>
2016-09-30 08:17:51 -0700

    migrations for djang default

---
+++ chatterbot/ext/django_chatterbot/admin.new.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from chatterbot.ext.django_chatterbot.models import Statement, Response, ChatUser, Score
+
+
+@admin.register(Statement)
+class StatementAdmin(admin.ModelAdmin):
+    list_display = ('text', 'score', 'chat_user', 'created', 'modified', )
+    list_filter = ('text', 'score', 'chat_user', 'created', 'modified', )
+    search_fields = ('text', 'score', 'chat_user', 'created', 'modified', )
+
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('response', 'prompt', 'occurrence', 'created', 'modified', )
+    list_filter = ('response', 'prompt', 'occurrence', 'created', 'modified', )
+    search_fields = ('response', 'prompt', 'occurrence', 'created', 'modified', )
+
+
+@admin.register(ChatUser)
+class ChatUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'full_name', 'gender', 'chat_age', 'created', 'modified', )
+
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('kindness', 'positive', 'negative', 'neutral', 'readability', 'flesch', 'kincaid', 'dale_chall', 'sarcasm', 'chat_age')
---
+++ chatterbot/ext/django_chatterbot/models.new.py
@@ -0,0 +1,157 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class ChatUser(BaseModel):
+    """ A short (<255) chat message, tweet, forum post, etc"""
+
+    user = models.ForeignKey(User, null=False)
+    username = models.CharField(
+        unique=True,
+        blank=False,
+        null=False,
+        max_length=64,
+    )
+    full_name = models.CharField(
+        blank=True,
+        null=False,
+        max_length=128,
+    )
+    gender = models.CharField(
+        help_text="Categorical variable to facilitate personality learning",
+        blank=True,
+        null=False,
+        max_length=32,
+    )
+    chat_age = models.FloatField(
+        help_text="Scalar between 0 and 100 estimating age of a user based solely on their chat style, word choice.",
+        blank=True,
+        null=True,
+        max_length=32,
+    )
+    kindness = models.FloatField(
+        help_text="Probability (0-1) of making a kind statement",
+        default=0.,
+        null=False,
+        blank=True,
+    )
+    hurtfulness = models.FloatField(
+        help_text="Probability (0-1) of making an unkind, cruel, hurtful statement",
+        default=0.
+    )
+
+    def __str__(self):
+        return '{}: {}'.format(self.username, self.full_name)
+
+
+class Score(BaseModel):
+    """Multidimensional score including sentiment (valence and intensity), readability, topic vectors, slangness, age, etc"""
+
+    # VADER components
+    positive = models.FloatField(default=None, null=True, blank=True,
+        help_text="VADER positive valence (emotion) intensity in a statement (see Hutto, C.J. & Gilbert)",
+        )
+    negative = models.FloatField(default=None, null=True, blank=True,
+        help_text="VADER negative valence (emotion) intensity in a statement (see Hutto, C.J. & Gilbert)",
+        )
+    neutral = models.FloatField(default=None, null=True, blank=True,
+        help_text="VADER neutral valence (emotion) intensity in a statement (see Hutto, C.J. & Gilbert)",
+        )
+    compound = models.FloatField(default=None, null=True, blank=True,
+        help_text="VADER compound valence intensity from NLTK.SentimentIntensityAnalyzer (see Hutto, C.J. & Gilbert)",
+        )
+    flesch = models.FloatField(default=None, null=True, blank=True,
+        help_text='Flesch Reading Ease based on sylable, word, and sentence counts (from `readability.superficial_measures["readability grades"]`).',
+        )
+    kincaid = models.FloatField(default=None, null=True, blank=True,
+        help_text='Kincaid grade level (from `readbility.superficial_measures["readability grades"]`)',
+        )
+    dale_chall = models.FloatField(default=None, null=True, blank=True,
+        help_text='Readability percent, 0-100, based on occurrence of 3000 most common English words (from `textstat.dale_chall_readability_score`)',
+        )
+    intensity = models.FloatField(default=None, null=True, blank=True,
+        help_text="Emotional intensity (from VADER by Hutto, C.J. & Gilbert)",
+        )
+    kindness = models.FloatField(default=None, null=True, blank=True,
+        help_text="Probability (0-1) of making a kind statement",
+        )
+    sarcasm = models.FloatField(default=None, null=True, blank=True,
+        help_text="Lack of sincerity in a statement",
+        )
+    readability = models.FloatField(default=None, null=True, blank=True,
+        help_text="Probability (0-1) of making a kind statement",
+        )
+    chat_age = models.FloatField(default=None, null=True, blank=True,
+        help_text="Oldfashioned-ness, negative hipness, in mean age of the utterance distribution in time",
+        )
+
+    def __str__(self):
+        s = 'emotion: ({:.2f} +{:.2f} -{:.2f}) * {:.2f}'.format(self.neutral or 0, self.positive or 0, self.negative or 0, self.intensity or 0)
+        s += ', readability: ({:.1f} {:.1f} {:.1f})'.format(self.flesch or 0, self.kincaid or 0, self.dale_chall or 0)
+        s += ', kindness: (+{} -{})'.format(self.kindness or 0, self.sarcasm or 0)
+        return s
+
+
+class Statement(BaseModel):
+    """A short (<255) chat message, tweet, forum post, etc"""
+
+    chat_user = models.ForeignKey(ChatUser, null=True)
+    score = models.OneToOneField(
+        Score, on_delete=models.CASCADE, primary_key=True)
+    score = models.ForeignKey(Score, null=True)
+    text = models.CharField(max_length=255)
+    responses = models.ManyToManyField('self', through='Response', symmetrical=False)
+
+    def __str__(self):
+        if len(self.text.strip()) > 128:
+            return '{}...'.format(self.text[:125])
+        elif len(self.text.strip()) > 0:
+            return self.text
+        return '<empty>'
+
+
+class Response(BaseModel):
+    """Connection from a prompting statement to the response that it invoked
+
+    Comparble to a ManyToMany "through" table, but without the M2M indexing/relations.
+
+    Only the text and number of times it has occurred are currently stored.
+    Might be useful to store additional features like language, location(s)/region(s),
+    first created datetime(s), username, user full name, user gender, etc.
+    A the very least occurrences should be an FK to a meta-data table with this info.
+    """
+
+    prompt = models.ForeignKey(
+        Statement,
+        verbose_name='Prompting statement',
+        related_name='in_response_to',  # "+" prevents link to prompting statement from response statement
+    )
+
+    # Response Statements can point to prompt Statements (prompts rel above)
+    #  but Prompt Statements cant point to responses (due to "+" below)
+    response = models.ForeignKey(
+        Statement,
+        verbose_name='Response statement',
+    )
+
+    occurrence = models.PositiveIntegerField(
+        help_text='Number of times this statement has been used to respond to the statement in `in_response_to`.',
+        default=0,
+        )
+
+    def __str__(self):
+        s = self.response.text if len(self.response.text) <= 64 else self.response.text[:61] + '...'
+        s += ' => '
+        s = self.prompt.text if len(self.prompt.text) <= 32 else self.prompt.text[:29] + '...'
+        return s
+
+    class Meta:
+        unique_together = (('prompt', 'response'),)
